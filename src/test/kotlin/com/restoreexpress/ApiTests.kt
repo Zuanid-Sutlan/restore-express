2a4b19dcee4a04e13213e6294885357005f40eee
zunaid.sultan <zunaid.sultan@appetiser.com.au>
2026-08-29 14:37:14 +0500

    fix the test failed problem

--- src/test/kotlin/com/restoreexpress/ApiTests.kt
+++ src/test/kotlin/com/restoreexpress/ApiTests.kt
@@ -276,12 +276,11 @@ class ApiTests {
         val versionService = VersionService()
         val info = versionService.getVersionInfo()
         
+        val expectedSuffix = if (info.isProduction) "prod" else "dev"
+        assertEquals("v${info.version}-$expectedSuffix", info.fullVersionTag)
+        assertFalse(info.fullVersionTag.contains("dev-dev"), "Dev tag should not have duplicated -dev-dev")
         if (info.isProduction) {
-            assertEquals("v1.0.0-prod", info.fullVersionTag)
             assertFalse(info.fullVersionTag.endsWith("-dev"), "Production tag should not end with -dev")
-        } else {
-            assertEquals("v1.0.0-dev", info.fullVersionTag)
-            assertFalse(info.fullVersionTag.contains("dev-dev"), "Dev tag should not have duplicated -dev-dev")
         }
     }
 
